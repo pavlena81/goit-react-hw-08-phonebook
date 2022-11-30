fc55d0d6cfb00f3fcac34d5733bb922ce1e5cf21
Olena Nesteruk <pavlena81@gmail.com>
2022-11-30 22:38:02 +0200

    Update LoginForm.js

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,8 +2,7 @@ import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
 import { FormLogin, LabelLogin, InputLogin, BtnLogForm } from './LoginForm.styled';
 
-import {ToastContainer,toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
@@ -11,13 +10,9 @@ export const LoginForm = () => {
   const handleSubmit = e => {
     e.preventDefault();
 
-    const form = e.currentTarget.elements;
-    if (form.email.value === '' ||
-     form.password.value === '') {
-      return toast.warning('please fill an all fields')
-      
-    }
-
+    const form = e.currentTarget;
+    
+   
     dispatch(
       logIn({
         email: form.elements.email.value,
@@ -38,7 +33,7 @@ export const LoginForm = () => {
         <InputLogin type="password" name="password" />
       </LabelLogin>
        <BtnLogForm type="submit">Log In</BtnLogForm>
-       <ToastContainer />
+       
     </FormLogin>
   );
 }; 
