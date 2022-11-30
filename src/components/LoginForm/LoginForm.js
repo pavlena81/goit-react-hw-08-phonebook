6d89034a9a7c3c7b382ad3f435352fe148f7d3dc
Olena Nesteruk <pavlena81@gmail.com>
2022-11-30 22:13:38 +0200

    add formValidation

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,12 +2,22 @@ import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
 import { FormLogin, LabelLogin, InputLogin, BtnLogForm } from './LoginForm.styled';
 
+import {ToastContainer,toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
-    const form = e.currentTarget;
+
+    const form = e.currentTarget.elements;
+    if (form.email.value === '' ||
+     form.password.value === '') {
+      return toast.warning('please fill an all fields')
+      
+    }
+
     dispatch(
       logIn({
         email: form.elements.email.value,
@@ -27,7 +37,8 @@ export const LoginForm = () => {
         Password
         <InputLogin type="password" name="password" />
       </LabelLogin>
-      <BtnLogForm type="submit">Log In</BtnLogForm>
+       <BtnLogForm type="submit">Log In</BtnLogForm>
+       <ToastContainer />
     </FormLogin>
   );
 }; 
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,12 +1,26 @@
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operations';
+
+import {ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 import { FormRegister, LabelRegister, InputRegister, BtnRegForm } from './RegisterForm.styled';
+
 export const RegisterForm = () => {
   const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
-    const form = e.currentTarget;
+
+    const form = e.currentTarget.elements;
+
+    if (form.name.value ==='' ||
+      form.email.value === '' ||
+     form.password.value === '') {
+      return toast.warning('please fill an all fields')
+      
+    }
+
     dispatch(
       register({
         name: form.elements.name.value,
@@ -31,7 +45,9 @@ export const RegisterForm = () => {
         Password
         <InputRegister  type="password" name="password" />
       </LabelRegister>
-      <BtnRegForm type="submit">Register</BtnRegForm>
-    </FormRegister>
+        <BtnRegForm type="submit">Register</BtnRegForm>
+        <ToastContainer />
+      </FormRegister>
+      
   );
 };
