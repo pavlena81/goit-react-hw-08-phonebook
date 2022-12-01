9ab17e13b9b67b8866e5253d9f40b97d0f7f1d12
Olena Nesteruk <pavlena81@gmail.com>
2022-12-01 22:05:43 +0200

    adds stylesToAppBar

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -3,9 +3,24 @@ import styled from "styled-components";
 
 
 export const Header = styled.div`
+  // display: flex;
+  // width:100%;
+  // justify-content: space-between;
+  // align-items: center;
+
+  top: 0;
+  left: 0;
+  position: sticky;
+  z-index: 1100;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  min-height: 50px;
+  padding-right: 20px;
+  padding-left: 20px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+
   margin-bottom: 70px;
   border-bottom: 1px solid #7FB3D5;
   box-shadow: 6px 2px 10px -1px rgba(127, 179, 213, 1);
