40f7167bda0ec1449f65c360d74d89794937cfa1
pavlena81 <99089157+pavlena81@users.noreply.github.com>
2022-12-03 16:39:45 +0000

    Deploying to gh-pages from @ pavlena81/goit-react-hw-08-phonebook@845a370ae3c1e7c004bf14209173c70caaca1107 🚀

---
+++ static/js/922.42b9d175.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{8922:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsxs)("main",{style:{textAlign:"center"},children:[(0,n.jsx)("b",{style:{fontSize:64},children:"404"}),(0,n.jsx)("p",{children:"Sorry, server cannot find the resource :("})]})}}}]);
+//# sourceMappingURL=922.42b9d175.chunk.js.map
